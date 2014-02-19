386186770e6359a43764eab5245e6949abd19283
balalay12 <balalay12@yandex.ru>
2014-02-19 21:23:29 +0600

    litle refactor and template

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -3,8 +3,43 @@
 <head>
     <meta charset="utf-8">
     <title>Главная</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="../../css/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <?php include 'application/views/'.$content_view; ?>
+    <!-- Navigation -->
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Project name</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+
+    <div class="container">
+        <!-- Подключаем контент -->
+        <?php include 'application/views/'.$content_view; ?>
+    </div>
+
+    <!-- Scripts -->
+    <script src="../../js/bootstrap/js/bootstrap.min.js"></script>
+    <!-- jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 </body>
 </html>
